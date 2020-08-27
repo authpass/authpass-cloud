494a1821d4d697c5ed30d73b9ea4ebad6291b35d
Herbert Poul <herbert@poul.at>
2020-08-27 15:44:36 +0200

    added appcast generate script.

---
+++ packages/artifact_server/appcast.generate.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -xeu
+
+basedir="${0%/*}"
+
+pushd "${basedir}"
+
+dir=artifacts
+
+prefix='AuthPass-setup-'
+
+latest=`cat ${dir}/${prefix}latest.exe.txt`
+FILENAME="${prefix}${latest}.exe"
+PUB_DATE=`date`
+split=($(echo $latest | tr '_' '\n'))
+
+BUILDNUMBER=${split[1]}
+VERSION_STRING=${split[0]}+${split[1]}
+
+if ! test -f "${dir}/${FILENAME}" ; then
+    echo "Unable to find file ${FILENAME}"
+    exit 1
+fi
+
+cat <<EOF > $dir/appcast.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:sparkle="http://www.andymatuschak.org/xml-namespaces/sparkle">
+    <channel>
+        <title>AuthPass Appcast</title>
+        <description>Most recent updates to AuthPass for Windows</description>
+        <language>en</language>
+        <item>
+            <title>Version $VERSION_STRING</title>
+            <sparkle:releaseNotesLink>
+                https://raw.githubusercontent.com/authpass/authpass/master/authpass/CHANGELOG.md
+            </sparkle:releaseNotesLink>
+            <pubDate>${PUB_DATE}</pubDate>
+            <enclosure url="https://data.authpass.app/data/artifacts/${FILENAME}"
+                sparkle:version="${BUILDNUMBER}"
+                sparkle:shortVersionString="${VERSION_STRING}"
+                sparkle:os="windows"
+                length="0"
+                sparkle:installerArguments="/SILENT /SP- /NOICONS"
+                       type="application/octet-stream" />
+        </item>
+    </channel>
+</rss>
+EOF
+
