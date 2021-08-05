d1000a9842014d875a0ce37d0163911e57688b79
Herbert Poul <herbert@codeux.design>
2021-08-05 14:47:37 +0200

    make email confirmation page a bit nicer.

--- packages/authpass_cloud_backend/lib/src/endpoint/email_confirmation.dart
+++ packages/authpass_cloud_backend/lib/src/endpoint/email_confirmation.dart
@@ -1,6 +1,10 @@
 import 'package:authpass_cloud_backend/src/env/env.dart';
 
-String emailConfirmationPage(Env env, String? token) {
+String pageScaffold(
+  Env env, {
+  String Function()? script,
+  required String Function() body,
+}) {
   // language=html
   return '''<!DOCTYPE html>
 <head>
@@ -8,12 +12,17 @@ String emailConfirmationPage(Env env, String? token) {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>AuthPass: Confirm email address</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css">
-<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-91100035-7"></script>
 <script>
-function onReturnCallback() {
-  document.forms[0].submit();
-}
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-91100035-7');
 </script>
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+${script?.call() ?? ''}
 <style>
 body {
 background-color: white;
@@ -25,15 +34,39 @@ background-color: white;
   <div class="modal-background"></div>
   <div class="modal-content">
   <div class="box content">
+    ${body()}
+  </div>
+  </div>
+</div>
+</body>
+  ''';
+}
+
+String emailConfirmationPage(Env env, String? token) {
+  return pageScaffold(
+    env,
+    script: () => '''
+<script>
+function onReturnCallback() {
+  document.forms[0].submit();
+}
+</script>
+''',
+    body: () => '''
     <form action="?" method="POST">
       <input type="hidden" name="token" value="$token" />
       <h2>Confirm your email address</h2>
       <p>Click the checkbox to confirm your email address.</p>
       <div class="g-recaptcha" data-sitekey="${env.config.secrets.recaptchaSiteKey}" data-callback="onReturnCallback"></div>
     </form>
-    </div>
-  </div>
-</div>
-</body>
-  ''';
+  ''',
+  );
+}
+
+String emailConfirmationSuccess(Env env) {
+  return pageScaffold(env,
+      body: () => ''' 
+<p>Successfully confirmed your email address. Please go back to AuthPass app. 
+You are now successfully authenticated.</p>
+''');
 }
