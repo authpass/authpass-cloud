fda113a6ad8dd60bc6965d78be9603f1bd3e42ef
Herbert Poul <herbert@codeux.design>
2021-08-22 22:05:14 +0200

    use `coverage` instead of `test_coverage`

--- _tools/test-coverage.sh
+++ _tools/test-coverage.sh
@@ -8,11 +8,15 @@ cd packages/authpass_cloud_backend
 
 pub get
 
-pub global activate test_coverage
+pub global activate coverage
 
 fail=false
-pub global run test_coverage || fail=true
+dart test --coverage coverage || fail=true
 echo "fail=$fail"
+
+jq -s '{coverage: [.[].coverage] | flatten}' $(find coverage -name '*.json' | xargs) > coverage/merged_json.cov
+pub global run coverage:format_coverage --packages=.packages -i coverage/merged_json.cov -l --report-on lib --report-on test > coverage/lcov.info
+
 bash <(curl -s https://codecov.io/bash) -f coverage/lcov.info
 
 test "$fail" == "true" && exit 1
