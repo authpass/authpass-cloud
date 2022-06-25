f1453a19ae1581479fbb2da31c12cc971b4a6a44
Herbert Poul <herbert@codeux.design>
2022-06-25 09:07:32 +0200

    fix coverage script

--- _tools/test-coverage.sh
+++ _tools/test-coverage.sh
@@ -6,16 +6,16 @@ cd "${0%/*}"/..
 
 cd packages/authpass_cloud_backend
 
-pub get
+dart pub get
 
-pub global activate coverage
+dart pub global activate coverage
 
 fail=false
 dart test --coverage coverage || fail=true
 echo "fail=$fail"
 
 jq -s '{coverage: [.[].coverage] | flatten}' $(find coverage -name '*.json' | xargs) > coverage/merged_json.cov
-pub global run coverage:format_coverage --packages=.packages -i coverage/merged_json.cov -l --report-on lib --report-on test > coverage/lcov.info
+dart pub global run coverage:format_coverage -i coverage/merged_json.cov -l --report-on lib --report-on test > coverage/lcov.info
 
 bash <(curl -s https://codecov.io/bash) -f coverage/lcov.info
 
